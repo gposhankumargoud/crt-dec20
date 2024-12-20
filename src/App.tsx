448d736f21f23bae036d414edec73240c99f2593
gposhankumargoud <gposhankumargoud@gmail.com>
2024-12-20 12:23:29 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Products from './Components/Products'
+import EmpSal from './Components/empsal'
+import EmpApi from './Components/Empapi'
+export default function App() {
+  return (
+    <div><EmpApi/><Products/><EmpSal/> </div>
+  )
+}
